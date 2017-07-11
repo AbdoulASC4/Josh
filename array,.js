a48c39e9bcedd1e0dfeba62c4422cd455248b0b6
josh5agee <josh5agee@hotmail.com>
2017-07-11 13:55:32 -0400

    .

---
+++ array,.js
@@ -0,0 +1,3 @@
+var myArray = [ "bannana", 22, true, "superb", 33 , 44, "the train came late today"];
+myArray.length;
+myArray.pop;
---
+++ pokemonArray2.js
@@ -0,0 +1,4 @@
+mypokemonroster = ["pikachu","magikarp","blastoise", "charizard", "mewtwo"];
+for(var i = 4 ; i > -1; i--){
+    console.log(mypokemonroster[i]);
+}
